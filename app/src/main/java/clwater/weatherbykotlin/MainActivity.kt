0cd4f80843519c51f658837a9bbcd7d3152004bb
clwater <gengzhibo@gengzhibo.net>
2017-07-27 11:31:20 +0800

    构造方法和函数

--- app/src/main/java/clwater/weatherbykotlin/MainActivity.kt
+++ app/src/main/java/clwater/weatherbykotlin/MainActivity.kt
@@ -1,14 +1,38 @@
 package clwater.weatherbykotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.jvm.javaClass
+import kotlin.jvm.javaClass
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "hello k"
+
+        message.text = add2(2 , 3).toString()
+
+        nicetoast("hello")
+//        nicetoast("hello2" , "MyTag")
+//        nicetoast("hello" , "MyTag" , Toast.LENGTH_SHORT)
+
     }
+
+    fun add( x : Int , y : Int) : Int{
+        return  x + y
+    }
+
+    fun add2( x: Int , y: Int) :Int = x + y
+
+//    无法引入1.0已经弃用javaClass<T>
+    fun nicetoast(message : String, tag :String = String::class.java.toString(), length : Int = Toast.LENGTH_SHORT){
+        Toast.makeText(this, "[$tag] $message" ,length).show()
+    }
+
 }
---
+++ app/src/main/java/clwater/weatherbykotlin/Person.kt
@@ -0,0 +1,11 @@
+package clwater.weatherbykotlin
+
+/**
+ * Created by gengzhibo on 17/7/26.
+ */
+
+open class Animal(name:String ){
+}
+
+class Person(name:String , surnanme:String ) :Animal(name){}
+
