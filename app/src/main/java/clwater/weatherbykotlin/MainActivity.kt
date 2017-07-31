670abe19aec9b90f1dde6618f500da32329163dc
clwater <gengzhibo@gengzhibo.net>
2017-07-31 15:11:19 +0800

    20170731

--- app/src/main/java/clwater/weatherbykotlin/MainActivity.kt
+++ app/src/main/java/clwater/weatherbykotlin/MainActivity.kt
@@ -2,11 +2,21 @@ package clwater.weatherbykotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import clwater.weatherbykotlin.EventBus.EB_Test
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
 }
+
